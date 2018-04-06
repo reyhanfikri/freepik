716afb271a612b9570e8b288eb445fd05a1472ae
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-06 20:52:15 +0700

    Membuat laman register sementara

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -121,6 +121,70 @@ class Account extends CI_Controller {
 
 	public function register_sementara()
 	{
-		$this->load->view('v_register_sementara');
+		if ($this->input->post('submit') !== null){
+
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			$data['username'] = "";
+
+			if ($username == "" && $password == ""){
+
+				$data['errorUsername'] = "Username belum diisi";
+				$data['errorPassword'] = "Password belum diisi";
+				$this->load->view('v_login_sementara', $data);
+
+			}else if ($username == ""){
+
+				$data['errorUsername'] = "Username belum diisi";
+				$data['errorPassword'] = "";
+				$this->load->view('v_login_sementara', $data);
+
+			}else if ($password == ""){
+
+				$data['errorUsername'] = "";
+				$data['errorPassword'] = "Password belum diisi";
+				$data['username'] = $username;
+				$this->load->view('v_login_sementara', $data);
+
+			}else{
+
+				$validation = $this->UserAccountModel->login($username, $password);
+
+				if ($validation == "Username tidak ditemukan"
+					|| $validation == "Password tidak benar"){
+
+					$data['errorUsername'] = $validation;
+					$data['errorPassword'] = "";
+					$data['username'] = $username;
+					$this->load->view('v_login_sementara', $data);
+
+				}else {
+
+					$this->UserAccountModel->setCookie($username);
+
+					$user_data = $this->UserAccountModel->getUserData($cookie);
+
+					if ($user_data->role == "user"){
+
+						redirect(base_url());
+
+					}else {
+
+						redirect(base_url()."admin");
+
+					}
+
+				}
+
+			}
+
+		}else{
+
+			$data['errorUsername'] = "";
+			$data['errorPassword'] = "";
+			$data['username'] = "";
+			$this->load->view('v_register_sementara', $data);
+
+		}
 	}
 }
---
+++ application/views/v_register_sementara.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register Percobaan</title>
+</head>
+<body>
+	<form action="<?php echo base_url()."register_sementara"; ?>" method="post">
+		<?php
+			echo $errorUsername; 
+			if ($errorUsername != ""){
+				echo "<br>";
+			}
+		?>
+		Username: <input type="text" name="username" value="<?php echo $username; ?>"><br><br>
+		<?php 
+			echo $errorPassword;
+			if ($errorPassword != ""){
+				echo "<br>";
+			}
+		?>
+		Password: <input type="password" name="password"><br><br>
+		<input type="submit" name="submit"><br>
+	</form>
+</body>
+</html>

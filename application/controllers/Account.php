8f52b09c79892e0a5117dfbeef1d7e3e80e9dffd
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-06 16:51:07 +0700

    Membuat error untuk login
    Memakai login_sementara

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -20,7 +20,42 @@ class Account extends CI_Controller {
 
 	public function login_sementara()
 	{
-		$this->load->view('v_login_sementara');
+		if ($this->input->post('submit') !== null){
+
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+
+			if ($username == "" && $password == ""){
+
+				$data['errorUsername'] = "Username belum diisi";
+				$data['errorPassword'] = "Password belum diisi";
+				$this->load->view('v_login_sementara', $data);
+
+			}else if ($username == ""){
+
+				$data['errorUsername'] = "Username belum diisi";
+				$data['errorPassword'] = "";
+				$this->load->view('v_login_sementara', $data);
+
+			}else if ($password == ""){
+
+				$data['errorUsername'] = "";
+				$data['errorPassword'] = "Password belum diisi";
+				$this->load->view('v_login_sementara', $data);
+
+			}else{
+
+				redirect(base_url());
+
+			}
+
+		}else{
+
+			$data['errorUsername'] = "";
+			$data['errorPassword'] = "";
+			$this->load->view('v_login_sementara', $data);
+
+		}		
 	}
 
 	public function register_sementara()
---
+++ application/views/v_login_sementara.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Percobaan</title>
+</head>
+<body>
+	<form action="<?php echo base_url()."login_sementara"; ?>" method="post">
+		<?php
+			echo $errorUsername; 
+			if ($errorUsername != ""){
+				echo "<br>";
+			}
+		?>
+		Username: <input type="text" name="username"><br><br>
+		<?php 
+			echo $errorPassword;
+			if ($errorPassword != ""){
+				echo "<br>";
+			}
+		?>
+		Password: <input type="password" name="password"><br><br>
+		<input type="submit" name="submit"><br>
+	</form>
+</body>
+</html>

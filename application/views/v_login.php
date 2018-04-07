e9687cb8fd962049920a066bd37b0bf79fd713eb
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-07 10:30:55 +0700

    Revisi sedikit untuk style login page

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -10,43 +10,42 @@
     <link href="assets/css/style.css" rel="stylesheet">
   </head>
   <body>
-  	<div class="col-md-3 col-md-offset-2 image">
-        <img src="assets/img/Picture1.png" style=width:300px;height:300px;">
-    </div>
-    <div class="col-md-offset-1 col-md-4 form-login">
-    
-    <?php
-    /* handle error */
-    if (isset($_GET['error'])) : ?>
-        <div class="alert alert-warning alert-dismissible" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <strong>Warning!</strong> <?=base64_decode($_GET['error']);?>
+    <div class="container">
+      	<div class="col-md-5" style="padding-left: 140px; padding-top: 165px; padding-bottom: 165px">
+            <img src="assets/img/Picture1.png" style=width:300px;height:300px;">
         </div>
-    <?php endif;?>
+        <div class="col-md-5 form-login" style="padding-left: 140px;">
+        
+        <?php
+        /* handle error */
+        if (isset($_GET['error'])) : ?>
+            <div class="alert alert-warning alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <strong>Warning!</strong> <?=base64_decode($_GET['error']);?>
+            </div>
+        <?php endif;?>
 
-        <div class="outter-form-login">
-        <div class="logo-login">
-        </div>
-            <form action="check-login.php" class="inner-login" method="post">
-            <h2 class="text-center title-login">User Login</h2>
-            <br>
-            	<center><b>Username</b></center>
-                <div class="form-group">
-                    <input type="text" class="form-control" name="username" placeholder="Username">
-                </div>
-            	<center><b>Password</b></center>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password">
-                </div>
+            <div class="outter-form-login">
+            <div class="logo-login">
+            </div>
+                <form action="check-login.php" class="inner-login" method="post">
+                <h2 class="text-center title-login">User Login</h2>
                 <br>
-                <input type="submit" class="btn btn-block btn-custom-green" value="Login" />
-            </form>
+                	<center><b>Username</b></center>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                    </div>
+                	<center><b>Password</b></center>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <br>
+                    <input type="submit" class="btn btn-block btn-custom-green" value="Login" />
+                </form>
+            </div>
         </div>
     </div>
-
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>

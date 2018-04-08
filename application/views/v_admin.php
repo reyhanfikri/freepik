75b6cce0012ad0046132aa912e83199b0224dc58
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-08 22:16:19 +0700

    Menambahkan bootstrap ke page v_admin

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Administrator Page</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FreePicture - ADMIN</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 	<button onclick="location.href='<?php echo site_url('logout'); ?>'">Logout</button>

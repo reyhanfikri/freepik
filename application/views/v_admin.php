e94b16137469543a9c8d2aec85c75721d56a8dd5
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-10 21:55:46 +0700

    Membuat tabel data user di page admin
    Mengcopy dari page admin_sementara

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -6,10 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FreePicture - ADMIN</title>
 
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo site_url('assets/css/bootswatch/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo site_url('assets/css/bootswatch/style.css') ?>" rel="stylesheet">
 </head>
 <body>
-	<button onclick="location.href='<?php echo site_url('logout'); ?>'">Logout</button>
+	<nav class="navbar navbar-expand-lg navbar-light navcolor" style="height: 60px;">
+	  <div class="container">
+		  <a class="navbar-brand" href="<?php echo site_url('admin') ?>"><img height="40px" width="40px" src="<?php echo site_url('assets/img/WebsiteLogo2.png') ?>"></a>
+		  <a class="navbar-brand" style="color: white; " href="<?php echo site_url('admin') ?>">FreePicture - Admin</a>
+
+		  <div class="collapse navbar-collapse" id="navbarColor01">
+		    <ul class="navbar-nav mr-auto">
+		    </ul>
+		      <button style="width: 100px; font-size: 18px;" class="btn btn-default" onclick="location.href='<?php echo site_url('logout'); ?>'">Logout</button>
+		  </div>
+	  </div>
+	</nav>
+	<br>
+	<div class="container">
+		<h2>Data User</h2>
+		<table class="table table-hover">
+			<thead>
+		    <tr>
+		      <th scope="col">No</th>
+		      <th scope="col">Email</th>
+		      <th scope="col">Username</th>
+		      <th scope="col">Action</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		  	<?php $i = 1; foreach ($data as $key) { ?>
+		  	  <tr>
+		  	  	<td><?php echo $i; ?></td>
+		  	  	<td><?php echo $key['email']; ?></td>
+		  	  	<td><?php echo $key['username']; ?></td>
+		  	  	<td><a href="<?php echo site_url('admin/hapus_user/'.$key['id']); ?>" class="btn btn-danger">HAPUS USER</a></td>
+		  	  </tr>	
+		  	<?php $i++; } ?>
+		  </tbody>
+		</table>
+	</div>
 </body>
 </html>

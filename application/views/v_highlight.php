a0451679067f75692e8eed6312114d3a3616f69e
reyhanfikri <yamato.reyhan123@gmail.com>
2018-04-27 18:48:47 +0700

    Mengubah title Home menjadi FreePicture

--- application/views/v_highlight.php
+++ application/views/v_highlight.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home</title>
+	<title>FreePicture</title>
 	<link href="<?php echo site_url('assets/css/bootswatch/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?php echo site_url('assets/css/bootswatch/style.css') ?>" rel="stylesheet">
     <link href="<?php echo site_url('assets/css/style2.css') ?>" rel="stylesheet">

187a5babbecf1ac841661ab0a01030ee452b303e
reyhanfikri <yamato.reyhan123@gmail.com>
2018-06-06 08:52:15 +0700

    Update database v7

--- assets/sql_database/db_freepik_v7.sql
+++ assets/sql_database/db_freepik_v7.sql
@@ -1,140 +1,262 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2018 at 06:30 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
- Source Server         : XAMPP
- Source Server Type    : MySQL
- Source Server Version : 100130
- Source Host           : localhost:3306
- Source Schema         : db_freepik
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100130
- File Encoding         : 65001
 
- Date: 05/06/2018 11:03:27
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `db_freepik`
+--
 
--- ----------------------------
--- Table structure for t_comment
--- ----------------------------
-DROP TABLE IF EXISTS `t_comment`;
-CREATE TABLE `t_comment`  (
-  `id_comment` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_comment`
+--
+
+CREATE TABLE `t_comment` (
+  `id_comment` int(11) NOT NULL,
   `id_gambar` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `comment` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id_comment`) USING BTREE,
-  INDEX `fk3`(`id_gambar`) USING BTREE,
-  INDEX `fk4`(`id_user`) USING BTREE,
-  CONSTRAINT `fk3` FOREIGN KEY (`id_gambar`) REFERENCES `t_gambar` (`id_gambar`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk4` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `comment` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Table structure for t_ekstensi_gambar
--- ----------------------------
-DROP TABLE IF EXISTS `t_ekstensi_gambar`;
-CREATE TABLE `t_ekstensi_gambar`  (
-  `id_ekstensi_gambar` int(11) NOT NULL AUTO_INCREMENT,
-  `ekstensi_gambar` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id_ekstensi_gambar`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of t_ekstensi_gambar
--- ----------------------------
-INSERT INTO `t_ekstensi_gambar` VALUES (1, 'jpg');
-INSERT INTO `t_ekstensi_gambar` VALUES (2, 'png');
+--
+-- Table structure for table `t_ekstensi_gambar`
+--
 
--- ----------------------------
--- Table structure for t_gambar
--- ----------------------------
-DROP TABLE IF EXISTS `t_gambar`;
-CREATE TABLE `t_gambar`  (
-  `id_gambar` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `t_ekstensi_gambar` (
+  `id_ekstensi_gambar` int(11) NOT NULL,
+  `ekstensi_gambar` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_ekstensi_gambar`
+--
+
+INSERT INTO `t_ekstensi_gambar` (`id_ekstensi_gambar`, `ekstensi_gambar`) VALUES
+(1, 'jpg'),
+(2, 'png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_gambar`
+--
+
+CREATE TABLE `t_gambar` (
+  `id_gambar` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_ekstensi_gambar` int(11) NOT NULL,
-  `nama_gambar` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `nama_file` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_gambar` varchar(100) NOT NULL,
+  `nama_file` varchar(200) NOT NULL,
   `jumlah_like` int(11) NOT NULL,
-  `jumlah_view` int(11) NOT NULL,
-  PRIMARY KEY (`id_gambar`) USING BTREE,
-  INDEX `fk2`(`id_user`) USING BTREE,
-  INDEX `fk_ekstensi_gambar`(`id_ekstensi_gambar`) USING BTREE,
-  CONSTRAINT `fk2` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_ekstensi_gambar` FOREIGN KEY (`id_ekstensi_gambar`) REFERENCES `t_ekstensi_gambar` (`id_ekstensi_gambar`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `jumlah_view` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of t_gambar
--- ----------------------------
-INSERT INTO `t_gambar` VALUES (9, 5, 1, 'Comedy_2', 'Comedy_2.jpg', 0, 1);
-INSERT INTO `t_gambar` VALUES (10, 6, 1, 'fantasy_background1', 'fantasy_background1.jpg', 0, 1);
-INSERT INTO `t_gambar` VALUES (11, 6, 1, 'cute_kitten1', 'cute_kitten1.jpg', 0, 1);
-INSERT INTO `t_gambar` VALUES (12, 6, 1, 'rumah', 'rumah.jpg', 0, 2);
+--
+-- Dumping data for table `t_gambar`
+--
 
--- ----------------------------
--- Table structure for t_role
--- ----------------------------
-DROP TABLE IF EXISTS `t_role`;
-CREATE TABLE `t_role`  (
+INSERT INTO `t_gambar` (`id_gambar`, `id_user`, `id_ekstensi_gambar`, `nama_gambar`, `nama_file`, `jumlah_like`, `jumlah_view`) VALUES
+(9, 5, 1, 'Comedy_2', 'Comedy_2.jpg', 0, 1),
+(10, 6, 1, 'fantasy_background1', 'fantasy_background1.jpg', 0, 1),
+(11, 6, 1, 'cute_kitten1', 'cute_kitten1.jpg', 0, 1),
+(12, 6, 1, 'rumah', 'rumah.jpg', 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_role`
+--
+
+CREATE TABLE `t_role` (
   `id_role` int(11) NOT NULL,
-  `role` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id_role`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of t_role
--- ----------------------------
-INSERT INTO `t_role` VALUES (1, 'user_biasa');
-INSERT INTO `t_role` VALUES (2, 'user_admin');
+--
+-- Dumping data for table `t_role`
+--
 
--- ----------------------------
--- Table structure for t_user
--- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `t_role` (`id_role`, `role`) VALUES
+(1, 'user_biasa'),
+(2, 'user_admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL,
   `id_role` int(11) NOT NULL,
-  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `username` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_role`(`id_role`) USING BTREE,
-  CONSTRAINT `fk_role` FOREIGN KEY (`id_role`) REFERENCES `t_role` (`id_role`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of t_user
--- ----------------------------
-INSERT INTO `t_user` VALUES (1, 2, 'admin@gmail.com', 'admin', 'admin');
-INSERT INTO `t_user` VALUES (3, 1, 'r@gmail.com', 'rrr', '654321');
-INSERT INTO `t_user` VALUES (5, 1, 'abc@gmail.com', 'abc', '123456');
-INSERT INTO `t_user` VALUES (6, 1, 'reyhanfikri@student.upi.edu', 'reyhanfikri', '123');
+--
+-- Dumping data for table `t_user`
+--
 
--- ----------------------------
--- Table structure for t_user_profile
--- ----------------------------
-DROP TABLE IF EXISTS `t_user_profile`;
-CREATE TABLE `t_user_profile`  (
-  `id_user_profile` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `t_user` (`id`, `id_role`, `email`, `username`, `password`) VALUES
+(1, 2, 'admin@gmail.com', 'admin', 'admin'),
+(3, 1, 'r@gmail.com', 'rrr', '654321'),
+(5, 1, 'abc@gmail.com', 'abc', '123456'),
+(6, 1, 'reyhanfikri@student.upi.edu', 'reyhanfikri', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_user_profile`
+--
+
+CREATE TABLE `t_user_profile` (
+  `id_user_profile` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `nama_lengkap` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `jenis_kelamin` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `alamat` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id_user_profile`) USING BTREE,
-  INDEX `fk_name`(`id_user`) USING BTREE,
-  CONSTRAINT `fk_name` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of t_user_profile
--- ----------------------------
-INSERT INTO `t_user_profile` VALUES (2, 3, NULL, NULL, NULL);
-INSERT INTO `t_user_profile` VALUES (4, 5, 'Atang Budi Cahyo', 'Laki-laki', 'Bojongsoang	                        ');
-INSERT INTO `t_user_profile` VALUES (5, 6, NULL, NULL, NULL);
+--
+-- Dumping data for table `t_user_profile`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `t_user_profile` (`id_user_profile`, `id_user`, `nama_lengkap`, `jenis_kelamin`, `alamat`) VALUES
+(2, 3, NULL, NULL, NULL),
+(4, 5, 'Atang Budi Cahyo', 'Laki-laki', 'Bojongsoang	                        '),
+(5, 6, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `t_comment`
+--
+ALTER TABLE `t_comment`
+  ADD PRIMARY KEY (`id_comment`),
+  ADD KEY `fk3` (`id_gambar`),
+  ADD KEY `fk4` (`id_user`);
+
+--
+-- Indexes for table `t_ekstensi_gambar`
+--
+ALTER TABLE `t_ekstensi_gambar`
+  ADD PRIMARY KEY (`id_ekstensi_gambar`);
+
+--
+-- Indexes for table `t_gambar`
+--
+ALTER TABLE `t_gambar`
+  ADD PRIMARY KEY (`id_gambar`),
+  ADD KEY `fk2` (`id_user`),
+  ADD KEY `fk_ekstensi_gambar` (`id_ekstensi_gambar`);
+
+--
+-- Indexes for table `t_role`
+--
+ALTER TABLE `t_role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indexes for table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_role` (`id_role`);
+
+--
+-- Indexes for table `t_user_profile`
+--
+ALTER TABLE `t_user_profile`
+  ADD PRIMARY KEY (`id_user_profile`),
+  ADD KEY `fk_name` (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `t_comment`
+--
+ALTER TABLE `t_comment`
+  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `t_ekstensi_gambar`
+--
+ALTER TABLE `t_ekstensi_gambar`
+  MODIFY `id_ekstensi_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `t_gambar`
+--
+ALTER TABLE `t_gambar`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `t_user_profile`
+--
+ALTER TABLE `t_user_profile`
+  MODIFY `id_user_profile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `t_comment`
+--
+ALTER TABLE `t_comment`
+  ADD CONSTRAINT `fk3` FOREIGN KEY (`id_gambar`) REFERENCES `t_gambar` (`id_gambar`),
+  ADD CONSTRAINT `fk4` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`);
+
+--
+-- Constraints for table `t_gambar`
+--
+ALTER TABLE `t_gambar`
+  ADD CONSTRAINT `fk2` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`),
+  ADD CONSTRAINT `fk_ekstensi_gambar` FOREIGN KEY (`id_ekstensi_gambar`) REFERENCES `t_ekstensi_gambar` (`id_ekstensi_gambar`);
+
+--
+-- Constraints for table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD CONSTRAINT `fk_role` FOREIGN KEY (`id_role`) REFERENCES `t_role` (`id_role`);
+
+--
+-- Constraints for table `t_user_profile`
+--
+ALTER TABLE `t_user_profile`
+  ADD CONSTRAINT `fk_name` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
